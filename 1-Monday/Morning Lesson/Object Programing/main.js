863854dcfe81cf523ef92d5fa563d14e108a9bc8
talbotcox <jontalbotcox@gmail.com>
2016-04-25 15:35:50 +0000

    first push into git for week 4

---
+++ 1-Monday/Morning Lesson/Object Programing/main.js
@@ -0,0 +1,10 @@
+(function(){
+    angular.module("MainModule", []) //Main Module Name
+    .controller("controllername", controllername); //Controller Name
+    
+function controllername (){ //Add controller name
+    var self = this; //Self Controller name in app
+    
+    
+}
+})()
